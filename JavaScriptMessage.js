424f7107663b30f8840f5ace63038c61ece67b70
marvokdolor <marvourneen@mkdolor.com>
2017-05-09 23:24:05 -0400

    Added a second message to JS file

--- JavaScriptMessage.js
+++ JavaScriptMessage.js
@@ -4,3 +4,10 @@ Feel free to experiment as well.
 Cheerio!
 Marvo
 */
+
+/* Second Message 5/9/2017:
+I just remembered that I actually wrote a blogpost about all the tools I've been using to learn to code: https://mkd16.tumblr.com/post/147940210677/why-an-environmentalist-should-learn-how-to-code
+I updated it this evening so it should have all the relevant info.
+Cheers,
+Marvo
+*/
